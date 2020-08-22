557ed31d1bce3248635857e27da8335c0534f719
Mikhail Zakharov <mikhailzakha9ov@gmail.com>
2020-08-22 16:18:28 +0300

    Start HW2.2 — go to sleep now.  I will reiterate later. Its true.

--- HW2.2/ViewController.swift
+++ HW2.2/ViewController.swift
@@ -10,11 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var showRGBView: UIView!
+    
+    @IBOutlet weak var redValue: UILabel!
+    @IBOutlet weak var greenValue: UILabel!
+    @IBOutlet weak var blueValue: UILabel!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Slider
+        redSlider.minimumValue = 0
+        redSlider.maximumValue = 1
+        
+        greenSlider.minimumValue = 0
+        greenSlider.maximumValue = 1
+        
+        blueSlider.minimumValue = 0
+        blueSlider.maximumValue = 1
+        
+        // Label
+        
     }
 
-
+    @IBAction func redSliderAction() {
+    }
+    
+    @IBAction func greenSliderAction() {
+    }
+    
+    @IBAction func blueSliderAction() {
+    }
+    
 }
 
